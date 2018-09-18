7aa18b843550e2beccac83e62410e00854359f04
Sean Wieler <sbwieler@gmail.com>
2018-09-18 10:55:32 -0700

    hello

--- myfirstplugin.php
+++ myfirstplugin.php
@@ -4,6 +4,15 @@ Plugin Name: My First Plugin
 */
 // [my_shortcode]
 function my_shortcode_func($atts) {
-    return "whatever text works";
+    ob_start();
+    ?>
+    <ul>
+        <li><a href="#thecariboo">The Cariboo</a></li>
+        <li><a href="#thechilcotin">The Chilcotin</a></li>
+        <li><a href="#thecoast">The Coast</a></li>
+    </ul>
+    <?php
+    $output = ob_get_clean();
+    return $output;
 }
 add_shortcode ('my_shortcode','my_shortcode_func');
