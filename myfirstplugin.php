2fffca430fee7e2df59eb37c97ba49996a766687
Sean Wieler <sbwieler@gmail.com>
2018-09-18 10:12:00 -0700

    first function

--- myfirstplugin.php
+++ myfirstplugin.php
@@ -2,3 +2,8 @@
 /*
 Plugin Name: My First Plugin
 */
+// [my_shortcode]
+function my_shortcode_func($atts) {
+    return "whatever text works";
+}
+add_shortcode ('myshortcode','my_shortcode_func');
