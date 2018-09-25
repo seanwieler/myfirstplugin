06c4f7e9693a8981bd602369461a330393dcfdd3
Sean Wieler <sbwieler@gmail.com>
2018-09-24 19:43:18 -0700

    api key added

--- myfirstplugin.php
+++ myfirstplugin.php
@@ -18,7 +18,7 @@ function my_footer_meddler() {
 	$maps_key = $settings->field( 'maps_key' );
 ?>
 <iframe width="600" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/view?zoom=13&center=48.4284%2C-123.3656&key=<?php echo $maps_key; ?>" allowfullscreen></iframe>
+src="https://www.google.com/maps/embed/v1/view?zoom=13&center=48.4284%2C-123.3656&key=AIzaSyBnNmJ5Fp8jPU7NHNCh_biLVI8UQ8tDooU" allowfullscreen></iframe>
 <?php
 }
 add_filter("the_content", "adding_text");
